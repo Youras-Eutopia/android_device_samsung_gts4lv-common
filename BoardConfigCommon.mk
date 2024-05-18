fafc69f4e4f570e8feca34469072622c8656cea6
Bruno Martins <bgcngm@gmail.com>
2024-05-18 13:21:42 +0100

    gts4lv-common: Use common libqti-perfd-client and power-libperfmgr

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -170,7 +170,7 @@ VENDOR_SECURITY_PATCH := 2022-11-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 PRODUCT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
--- gts4lv.mk
+++ gts4lv.mk
@@ -281,11 +281,12 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.lineage-libperfmgr \
     android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.0.vendor \
     vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    vendor.qti.hardware.perf@2.2.vendor \
+    libqti-perfd-client
 
 # QCOM
 PRODUCT_PACKAGES += \
@@ -320,7 +321,9 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/samsung
+    hardware/samsung \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Thermal
 PRODUCT_PACKAGES += \
